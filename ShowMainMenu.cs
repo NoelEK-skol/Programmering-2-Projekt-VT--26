ca4a01f62c12aa2c8fb21b8984e449d3efa9dab8
NoelEK-skol <noek07001@utb.vaxjo.se>
2026-01-27 18:29:15 +0100

    delat upp main menu i annan fil

---
+++ ShowMainMenu.cs
@@ -0,0 +1,102 @@
+partial class Program
+{
+    static void ShowMainMenu()
+    {
+        while (true)
+        {
+            Console.WriteLine("[1] Låna bok");
+            Console.WriteLine("[2] Lämna tillbaka bok");
+            Console.WriteLine("[3] Lägg till en bok");
+            Console.WriteLine("[4] Visa alla böcker");
+            Console.WriteLine("[5] Sök bok");
+            Console.WriteLine("________");
+            Console.WriteLine("[7] Logga ut");
+            Console.WriteLine("[8] Visa alla användare");
+            Console.WriteLine("[0] Avsluta");
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (input == 1)
+            {
+                Console.WriteLine("Låna bok");
+            }
+            if (input == 2)
+            {
+                Console.WriteLine("Lämna tillbaka bok");
+
+            }
+            if (input == 3)
+            {
+                Console.WriteLine("Lägg till en bok");
+                Console.Write("Titel: ");
+                string titel = Console.ReadLine()!;
+
+                Console.Write("Författare: ");
+                string författare = Console.ReadLine()!;
+
+                Console.Write("Genre: ");
+                string genre = Console.ReadLine()!;
+
+                try
+                {
+                    if (string.IsNullOrWhiteSpace(titel) || string.IsNullOrWhiteSpace(författare) || string.IsNullOrWhiteSpace(genre))
+                    {
+                        throw new ArgumentException("Alla fält måste fyllas i!");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                library.AddBook(new Bok(titel, författare, genre));
+                StreamWriter sw = new StreamWriter("Textfil.txt", true);
+                sw.WriteLine($"{dt1} {titel}, {författare}, {genre}");
+                sw.Close();
+                Console.WriteLine("Din bok har lagts till!");
+
+            }
+            if (input == 4)
+            {
+                Console.WriteLine("Visar alla böcker:");
+                StreamReader sr = new StreamReader("Textfil.txt");
+                Console.WriteLine(sr.ReadToEnd());
+                sr.Close();
+                Console.WriteLine("Antal böcker i biblioteket: " + library.GetAllBooks().Count); //Antal böcker i biblioteket under en körning
+                Console.WriteLine("[4] Tillbaka till menyn");
+                int VisaBöckerInput = Convert.ToInt32(Console.ReadLine());
+                if (VisaBöckerInput == 4)
+                {
+                    continue;
+                }
+            }
+            if (input == 5)
+            {
+                Console.WriteLine("Sök bok");
+            }
+            if (input == 0)
+            {
+                running = false;
+                break;
+            }
+            if (input < 1 || input > 9)
+            {
+                Console.WriteLine("Ogiltigt val, försök igen.");
+            }
+            if (input == 7)
+            {
+                loggedinUser = null!;
+                return;
+            }
+            if (input == 8)
+            {
+                Console.WriteLine($"Antal användare: {users.Count}");
+                Console.WriteLine("Alla användare:");
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"Användarnamn: {user.Användarnamn}");
+                }
+            }
+            
+        }
+    }
+}
--- obj/Debug/net9.0/Programmering-2-Projekt-VT--26.AssemblyInfo.cs
+++ obj/Debug/net9.0/Programmering-2-Projekt-VT--26.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Programmering-2-Projekt-VT--26")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c74b7551c55fbcf45ed24e53c9c0d02086e54b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb59c8949956c4573415e16ed273c983e460440")]
 [assembly: System.Reflection.AssemblyProductAttribute("Programmering-2-Projekt-VT--26")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Programmering-2-Projekt-VT--26")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
