5bb539bf01ec7682484703fc1bd2d3d196ddc8cc
NoelEK-skol <noek07001@utb.vaxjo.se>
2026-03-17 13:56:26 +0100

    fixat bugg med återlämning av bok

--- ReturnBook.cs
+++ ReturnBook.cs
@@ -2,6 +2,7 @@ partial class Program
 {
     static void ReturnBook()
     {
+        int svar;
         var myLoans = loans.Where(l => l.UserName == loggedinUser.Användarnamn).ToList();
         
         if(myLoans.Count == 0){
@@ -14,16 +15,26 @@ partial class Program
         {
             Console.WriteLine($"[{i + 1}] {myLoans[i].Book.Titel} av {myLoans[i].Book.Författare}");
         }
-        int svar = int.Parse(Console.ReadLine()!);
+        
+        bool nummer = int.TryParse(Console.ReadLine(), out svar);
+        if (!nummer)
+        {
+            Console.WriteLine("Ange en siffra");
+            return;
+        }
         if (svar < 1 || svar > myLoans.Count)
         {
             Console.WriteLine("Ogiltigt val");
             return;
         }
+
         Loan loan = myLoans[svar - 1];
         loan.ReturnBook();
         library.GetAllBooks().Add(loan.Book);
+        loans.Remove(loan);
+        SaveBook();
         SaveLoans();
+        
         Console.WriteLine($"Du har lämnat tillbaka {loan.Book.Titel} av {loan.Book.Författare}!");
     }
 
